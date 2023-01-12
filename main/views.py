1426df1b425d4d6228b5f407e50bbb23d0ef0f22
반민정 <minjeong19@naver.com>
2023-01-13 00:37:23 +0900

    VGG16으로 변경

--- main/views.py
+++ main/views.py
@@ -3,7 +3,9 @@ from .models import Musinsa_w, Musinsa_m, Profile
 import pandas as pd
 # Create your views here.
 from django.http import HttpResponse
-
+import numpy as np
+from numpy import dot
+from numpy.linalg import norm
 
 def index(request):
     return render(request, 'main/home.html')
@@ -83,7 +85,8 @@ def upload_create(request):
     form.save()
     img = form.image.url
     #특징벡터 추출
-    feature = get_vector('C:/shoes/mysite/' + str(img))
+    img1 = Image.open('C:/shoes/mysite/' + str(img))
+    feature = fe.extract(img1)
 
     #유사한 신발 찾기
     #1. 남성 무신사
@@ -93,7 +96,7 @@ def upload_create(request):
         if i in noshoes_m:
             pass
         else:
-            sim = cos_pytorch(feature, features_m_resnet[i])
+            sim = cos(feature, features_m_resnet[i])
             # 중복이미지 제거
             if sim in check:
                 pass
@@ -116,7 +119,7 @@ def upload_create(request):
         if i in noshoes_w:
             pass
         else:
-            sim = cos_pytorch(feature, features_w_resnet[i])
+            sim = cos(feature, features_w_resnet[i])
             if sim in check1:
                 pass
             else:
@@ -156,55 +159,33 @@ def upload_create(request):
 
 
 # 이미지 특징 추출 딥러닝 코드
-import torch
-import torch.nn as nn
-import torchvision.models as models
-import torchvision.transforms as transforms
-from torch.autograd import Variable
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.models import Model
+from pathlib import Path
 from PIL import Image
-from torchvision.models import resnet18, ResNet18_Weights
 
-# Load the pretrained model
-model = models.resnet18(weights=ResNet18_Weights.DEFAULT)
+class FeatureExtractor:
+  def __init__(self):
+        # Use VGG-16 as the architecture and ImageNet for the weight
+        base_model = VGG16(weights='imagenet')
+        # Customize the model to return features from fully-connected layer
+        self.model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
 
-# Use the model object to select the desired layer
-layer = model._modules.get('avgpool')
+  def extract(self, img):
+        # Resize the image
+        img = img.resize((224, 224))
+        # Convert the image color space
+        img = img.convert('RGB')
+        # Reformat the image
+        x = img
+        x = np.expand_dims(x, axis=0) #차원추가
+        x = preprocess_input(x) #모델에 필요한 형식에 이미지를 적절하게 맞추기위한 것
+        # Extract Features
+        feature = self.model.predict(x)[0]
+        return feature / np.linalg.norm(feature)
 
-# Set model to evaluation mode
-model.eval()
-
-
-def get_vector(image):
-    img = Image.open(image)
-    img = img.convert('RGB')
-    # 2. Create a PyTorch Variable with the transformed musinsa_image
-    # Image transforms
-    scaler = transforms.Resize((224, 224))
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    to_tensor = transforms.ToTensor()
-    t_img = Variable(normalize(to_tensor(scaler(img))).unsqueeze(0))
-    # 3. Create a vector of zeros that will hold our feature vector
-    #    The 'avgpool' layer has an output size of 512
-    my_embedding = torch.zeros([1, 512, 1, 512])
-
-    # 4. Define a function that will copy the output of a layer
-    def copy_data(m, i, o):
-        my_embedding.copy_(o.data)
-
-    # 5. Attach that function to our selected layer
-    h = layer.register_forward_hook(copy_data)
-    # 6. Run the model on our transformed musinsa_image
-    model(t_img)
-    # 7. Detach our copy function from the layer
-    h.remove()
-    # 8. Return the feature vector
-    return my_embedding
-
-def cos_pytorch(A, B):
-  cos= nn.CosineSimilarity(dim=1, eps=1e-6)
-  cos_sim = cos(A, B)
-  return cos_sim.numpy()[0][0][0]
-
+fe = FeatureExtractor()
 #무신사 데이터 셋의 특징벡터 추출
 #남성
 noshoes_m= [16, 26, 39,55,69,244,245,246,285,307,309,310,311,312,314,315,316,317,318,319,321,322,323,324,325,326,327,328,329,330,331,332,333,334,335,336,337,338,339,340,341,342,343,344,345,346,347,348,349,350,351,353,354,355,356,357,358,359,360,362,363,364,365,366,414,418,421]
@@ -214,7 +195,8 @@ for i in range(427):
   if i in noshoes_m:
     features_m_resnet.append(None)
   else:
-    fea = get_vector(f"C:/shoes/mysite/static/musinsa_image/man_shoes/man_{i}.jpg")
+    img = Image.open(f"C:/shoes/mysite/static/musinsa_image/man_shoes/man_{i}.jpg")
+    fea = fe.extract(img)
     features_m_resnet.append(fea)
 #여성
 noshoes_w = [47, 77, 134, 139, 148, 149, 157, 171, 220, 273, 313, 314, 315, 319, 321, 329, 342, 352, 353, 356, 357, 397, 404, 413, 415, 424, 427, 447, 461, 477, 479, 504, 510, 513, 527, 529, 530, 537, 541, 542, 544, 545, 547, 548, 549, 550, 551, 553, 554, 555, 557, 558, 559, 560, 561, 563, 565, 566, 569, 570, 573, 574, 575, 576, 578, 579, 582, 583, 584, 585, 587, 588, 589, 590, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 605, 606, 607, 609, 610, 611, 612, 613, 614, 615, 616, 617, 618, 620, 621, 622, 623, 624, 625, 626, 628, 629, 631, 632, 637, 638, 639, 644, 645, 647, 648, 649, 651, 652, 653, 654, 658, 659]
@@ -224,5 +206,10 @@ for i in range(660):
   if i in noshoes_w:
     features_w_resnet.append(None)
   else:
-    fea = get_vector(f"C:/shoes/mysite/static/musinsa_image/woman_shoes/woman_{i}.jpg")
+    img = Image.open(f"C:/shoes/mysite/static/musinsa_image/woman_shoes/woman_{i}.jpg")
+    fea = fe.extract(img)
     features_w_resnet.append(fea)
+
+
+def cos(A, B):
+    return dot(A, B) / (norm(A) * norm(B))
